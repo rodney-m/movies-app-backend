b79cd780ed70ac0315a52a860d1bdfa2eaf30450
Rodney Mupanduki <rodney@hisp.org>
2023-07-28 05:00:25 +0200

    refactored

--- routes/users.js
+++ routes/users.js
@@ -4,54 +4,60 @@ const router = express.Router();
 const bycrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-router.post("/", async (req, res) => {
-  let user = new User({
-    email: req.body.email,
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    passwordHash: bycrypt.hashSync(req.body.password, 10),
-  });
+router.post('/', async (req, res) => {
+  const { email, firstName, lastName, password } = req.body;
 
   const secret = process.env.secret;
 
-  // check if email already exists in the db
-  const userExists = await User.findOne({ email: req.body.email });
-
-  const createToken = () => {
-    const token = jwt.sign(
-      {
-        userId: user.id,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email,
-      },
-      secret,
-      { expiresIn: "1d" }
-    );
-    return token;
-  };
-
-  if (!userExists) {
-    user = await user.save();
-    if (!user) {
-      res.status(400).send("Failed to create user");
-    } else {
-      const token = createToken();
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      const token = createToken(userExists);
       res.status(200).send({
-        data: user,
-        message: "success",
-        token: token,
+        data: userExists,
+        message: 'success',
+        status: true,
+        token,
       });
+      return;
     }
-  } else {
-    const token = createToken();
-    res.status(200).send({
-      data: userExist,
-      message: "success",
-      token: token,
+
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      email,
+      firstName,
+      lastName,
+      passwordHash,
     });
+
+    const token = createToken(user);
+    res.status(200).send({
+      data: user,
+      message: 'success',
+      token,
+      status: true
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({message: 'Failed to create user', status: false});
   }
 });
+
+function createToken(user) {
+  const { id, firstName, lastName, email } = user;
+  const token = jwt.sign(
+    {
+      userId: id,
+      firstName,
+      lastName,
+      email,
+    },
+    secret,
+    { expiresIn: '1d' }
+  );
+  return token;
+}
 
 router.post("/login", async (req, res) => {
   try {
